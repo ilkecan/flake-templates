fa1020a0641d1dcd7da2b5ef8be11a420a876d7d
ilkecan <ilkecan@protonmail.com>
2021-11-11 19:58:58 +0300

    Init repo

---
+++ flake.nix
@@ -0,0 +1,14 @@
+{
+  description = "A collection of flake templates";
+
+  outputs = { self }: {
+    templates = {
+      trivial = {
+        path = ./trivial;
+        description = "A very basic flake";
+      };
+    };
+
+    defaultTemplate = self.templates.trivial;
+  };
+}
